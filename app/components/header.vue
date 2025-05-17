444fa2adbc45527df4ddd93425d711bbc0f9f258
phatnguyen <phatnguyen@gmx.de>
2025-05-17 11:31:02 +0200

    wip

--- app/components/header.vue
+++ app/components/header.vue
@@ -7,30 +7,38 @@ if (colorMode.preference === 'system') {
 
 <template>
   <div class="absolute top-0 right-0 flex gap-2">
-    <UButton
-      icon="i-lucide-newspaper"
-      variant="soft"
-      to="https://github.com/Robotics-TH-Ohm/Isovist-Report/blob/main/main.pdf"
-      target="_blank"
-      size="sm"
-    />
-    <UButton
-      icon="i-lucide-github"
-      variant="soft"
-      to="https://robotics-th-ohm.github.io/Isovist/"
-      target="_blank"
-      size="sm"
-    />
-    <UButton
-      :icon="colorMode.preference === 'dark' ? 'i-lucide-moon' : 'i-lucide-sun'"
-      variant="soft"
-      square
-      size="sm"
-      @click="colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'"
-    />
+    <UTooltip text="Papier">
+      <UButton
+        icon="i-lucide-newspaper"
+        variant="soft"
+        to="https://github.com/Robotics-TH-Ohm/Isovist-Report/blob/main/main.pdf"
+        target="_blank"
+        size="sm"
+      />
+    </UTooltip>
+
+    <UTooltip text="Github Repo">
+      <UButton
+        icon="i-lucide-github"
+        variant="soft"
+        to="https://robotics-th-ohm.github.io/Isovist/"
+        target="_blank"
+        size="sm"
+      />
+    </UTooltip>
+
+    <UTooltip :text="colorMode.preference === 'dark' ? 'Dark Mode' : 'Light Mode'">
+      <UButton
+        :icon="colorMode.preference === 'dark' ? 'i-lucide-moon' : 'i-lucide-sun'"
+        variant="soft"
+        square
+        size="sm"
+        @click="colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'"
+      />
+    </UTooltip>
   </div>
 
-  <h1 class="text-success font-bold text-xl mt-2 tracking-widest">
-    Kidnapped Robot and Isovist
+  <h1 class="text-success text-xl font-semibold">
+    Kidnapped Robot Problem & Isovisten
   </h1>
 </template>
